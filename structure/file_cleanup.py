37782c4d7ad3cd65accbbc8677d14fe74f39d1c1
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-02-12 14:37:50 -0300

    Cleanup of the save structure's text

--- structure/file_cleanup.py
+++ structure/file_cleanup.py
@@ -1,8 +1,12 @@
 import re
 
 """
-This Python script splits the CascalCase from the save file structure,
-by putting a space before a capital letter in the middle of a word.
+This Python script makes some a few changes on the text of the save structure
+in order to make it more user friendly:
+
+- splitting the CascalCase from the save file structure, by putting a space
+  before a capital letter in the middle of a word.
+- Replacing the 'Rn_' abbreviation by 'Round n '.
 """
 
 # Regular expression to match uppercased words that inside another word
@@ -10,10 +14,18 @@ by putting a space before a capital letter in the middle of a word.
 # ([A-Z]+[a-z]+)    One or more uppercase letters followed by one or more lowercase letters
 camel_case = re.compile(r"(?<=\B)([A-Z]+[a-z]+)")
 
+# Regular expression to match the 'Rn_' pattert (where n = 1,2,3)
+round_n = re.compile(r"R([1-3])_")
+
 # Loop through the file
 edited_lines = []
 with open("Save File Structure - 1.0.9.55.tsv", "rt") as file:
     for line in file:
+
+        # Replace 'Rn_' by 'Round n '
+        # (working only on lines 350 or 354 because it only happens there)
+        if line.startswith("350") or line.startswith("354"):
+            line = round_n.sub(r"Round \1 ", line)
 
         # Split the cells before the capital letters in the middle,
         # then join the parts by a space
