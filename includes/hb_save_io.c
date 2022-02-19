f88b1ad5cce9c1d6cae403872abfd8f3b0efb0d0
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-02-19 15:15:36 -0300

    Parsing the headers of the save structure file

--- includes/hb_save_io.c
+++ includes/hb_save_io.c
@@ -10,12 +10,14 @@
 #define PATH_BUFFER 512                         // Maximum number of characters of the absolute file path
 #define SAVE_LINE_BUFFER 50                     // Maximum number of characters on each line of the save file
 
-char SAVE_PATH[PATH_BUFFER];    // Absolute path to the save file
+extern char SAVE_PATH[PATH_BUFFER];    // Absolute path to the save file
 
 // Store the location of the save file on the SAVE_PATH variable
-void find_save()
+int find_save()
 {
     snprintf(SAVE_PATH, sizeof(SAVE_PATH), "%s\\%s\\%s", LOCAL_APP_DATA, SAVE_FOLDER, SAVE_FNAME);
+    return 0;
+    // TO DO: Error handling
 }
 
 // Get the contents of the save file and store them on memory
@@ -29,4 +31,7 @@ void read_save()
         fgets(line, sizeof(line), save_file);
         /* Do stuff with each line... */
     }
+
+    return 0;
+    // TO DO: Error handling
 }
--- includes/hb_save_struct.c
+++ includes/hb_save_struct.c
@@ -1,7 +1,15 @@
 /* Template and data structure for the saved data of Heartbound */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SAVE_STRUCT_LOC "lib\\save_structure.tsv"   // Path to the file with the save structure
+#define TEXT_BUFFER_SIZE (size_t)500                // Buffer size (in bytes) for each line of the structure file
+#define NUM_STORY_VARS (size_t)1000                 // Amount of storyline variables in the save file
+
 // Player attributes
-char *seed_code[10];            // Game seed (10 decimal characters long)
+char *game_seed[10];            // Game seed (10 decimal characters long)
 char *room_id[30];              // The ID (as a string) of the room the player is
 double x_axis, y_axis;          // Coordinates of the player in the room
 double hp_current, hp_maximum;  // Current and maximum hit points of the player
@@ -13,10 +21,77 @@ char known_glyphs;
 // The storyline variables
 struct StorylineVars
 {
-    unsigned int value;
-    char name[30];
-    char info[100];
-    char detail[50];
-    char max;
-    char **aliases;
-} save_data[1000];
+    unsigned int value;          // Value of this entry on the save file
+    char *location;              // In-game location that this entry applies to
+    char *name;                  // Description of the in-game feature this entry refers to
+    char *info;                  // Further description of the feature
+    char *unit;                  // Measurement unit of the value this entry represents
+    unsigned short num_entries;  // Amount of different values that the field accept (0 if it accepts any value)
+    char **aliases;              // Associate each numeric value to its meaning (as strings)
+} save_data[NUM_STORY_VARS];
+
+// Headers of the save structure
+char **save_headers;            // Names of the columns (array of strings)
+size_t num_columns;             // Amount of columns in the structure
+
+// Create the data structure for the save file
+int open_save()
+{
+    // Open the structure file
+    FILE *save_structure = fopen(SAVE_STRUCT_LOC, "r");
+
+    // Buffer for reading the lines of the file
+    char *line_buffer = malloc(TEXT_BUFFER_SIZE * sizeof(char));
+
+    // Get the table header and amount of columns
+    fgets(line_buffer, sizeof(line_buffer), save_structure);    // Read the first line
+    num_columns = 1;
+    for (size_t i = 0; i < TEXT_BUFFER_SIZE; i++)
+    {
+        if (line_buffer[i] == '\t') {num_columns++;}
+        else if (line_buffer[i] == '\n') {break;}
+    }
+
+    // Store the column names in an array
+    save_headers = malloc(num_columns * sizeof(char*));
+    char *name_buffer = malloc(TEXT_BUFFER_SIZE + (size_t)1);   // Buffer for the column name
+    size_t pos = (size_t)0;         // Current position of character on the line
+    size_t column = (size_t)0;      // Position of the current column
+    size_t col_size = (size_t)0;    // Amount of characters on the current column
+
+    while (line_buffer[pos] != '\n')    // Loop until the line break
+    {
+        if (column >= num_columns) break;       // Prevent overflow of the headers array
+        if (pos >= TEXT_BUFFER_SIZE) break;     // Prevent overflow of the text buffers
+        
+        // Add the current character to the name buffer
+        name_buffer[pos] = line_buffer[pos];
+        col_size++;
+
+        // Move to the next character and check if the column has ended
+        if (line_buffer[++pos] == '\t')
+        {
+            name_buffer[pos] = '\0';                        // Terminate the string (null terminator)
+            save_headers[column] = malloc( ++col_size );    // Allocate enough memory for the string (including the terminator)
+            strcpy(save_headers[column], name_buffer);      // Copy the string from the buffer until the terminator (inclusive)
+            column++;                                       // Move to the next column
+        }
+    }
+    
+    // Free the memory of the line buffer
+    free(line_buffer);
+    
+    return 0;
+    // TO DO: Error handling
+}
+
+// Free used memory for the save file
+void close_save()
+{
+    // Names of the columns of the save structure
+    for (unsigned short i = 0; i < num_columns; i++)
+    {
+        free(save_headers[i]);
+    }
+    free(save_headers);
+}
