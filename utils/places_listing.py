f46916c2768d7c53ab09d5b3fa7bd96dc0b684cf
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-03-08 21:11:57 -0300

    Renamed Places List file

--- utils/places_listing.py
+++ utils/places_listing.py
@@ -8,6 +8,6 @@ with open("..\structure\save_structure.tsv", "rt") as input_file:
     my_index = header.index("Room/Object")
     places = {line.split("\t")[my_index] + "\n" for line in input_file}
 
-with open("..\structure\places_list.tsv", "wt") as output_file:
+with open("..\structure\places_list-raw.tsv", "wt") as output_file:
     output_file.write("Room/Object")
     output_file.writelines(sorted(places))
