4e0480ead952cf0749a12913f25bff064441e78a
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-03-18 17:38:37 -0300

    Created program's icon

--- Heartbound Save Editor.c
+++ Heartbound Save Editor.c
@@ -4,44 +4,30 @@
 #include <hb_save.h>
 // #include <windows.h>
 
-static void
-activate (GtkApplication* app,
-          gpointer        user_data)
+static void activate( GtkApplication* app, gpointer user_data )
 {
-  GtkWidget *window;
-
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-  gtk_widget_show (window);
+    GtkWidget *window;
+    
+    window = gtk_application_window_new(app);
+    gtk_window_set_icon_from_file(GTK_WINDOW(window), "lib\\icon.png", NULL);
+    gtk_window_set_title( GTK_WINDOW(window), "Heartbound Save Editor" );
+    gtk_window_set_default_size( GTK_WINDOW(window), 720, 540 );
+    gtk_widget_show(window);
 }
 
-int
-main (int    argc,
-      char **argv)
+int main ( int argc, char **argv )
 {
-  chdir("C:\\Users\\Tiago\\Desktop\\C\\Projetos\\Heartbound Save Editor\\release");
-  // LONGLONG r = LONG_LONG_MAX;
-  // for (size_t i = 0; i < 10000; i++)
-  // {
-  //   LARGE_INTEGER start, end, freq;
-  //   QueryPerformanceFrequency(&freq);
-  //   QueryPerformanceCounter(&start);
+    chdir("C:\\Users\\Tiago\\Desktop\\C\\Projetos\\Heartbound Save Editor\\release");
     open_save();
-    // QueryPerformanceCounter(&end);
-    // LONGLONG total = end.QuadPart - start.QuadPart;
-    // r = (total < r) ? total : r;
     close_save();
-  // }
-  // printf("%d", r);
 
-  GtkApplication *app;
-  int status;
+    GtkApplication *app;
+    int status;
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
+    app = gtk_application_new( "com.github.tbpaolini.hbsaveedit", G_APPLICATION_FLAGS_NONE );
+    g_signal_connect( app, "activate", G_CALLBACK(activate), NULL );
+    status = g_application_run( G_APPLICATION(app), argc, argv );
+    g_object_unref(app);
 
-  return status;
+    return status;
 }
