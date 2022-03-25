6e1368b62633bb7c969d512cec6e88db748cbcfa
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-03-25 18:23:21 -0300

    Loaded location names and their image files into the window

--- main.c
+++ main.c
@@ -6,16 +6,25 @@
 #include <hb_save.h>
 // #include <windows.h>
 
+#define WINDOW_WIDTH 720
+#define WINDOW_HEIGHT 540
+#define WINDOW_BORDER 10
+#define WINDOW_TITLE "Heartbound Save Editor"
+#define WINDOW_ICON "..\\lib\\icon.png"
+
+#define IMAGE_WIDTH 80
+#define IMAGE_HEIGHT 80
+
 static void activate( GtkApplication* app, gpointer user_data )
 {
     GtkWidget *window;
     
     // Create the application window
     window = gtk_application_window_new(app);
-    gtk_window_set_icon_from_file(GTK_WINDOW(window), "..\\lib\\icon.png", NULL);
-    gtk_window_set_title( GTK_WINDOW(window), "Heartbound Save Editor" );
-    gtk_window_set_default_size( GTK_WINDOW(window), 720, 540 );
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+    gtk_window_set_icon_from_file(GTK_WINDOW(window), WINDOW_ICON, NULL);
+    gtk_window_set_title( GTK_WINDOW(window), WINDOW_TITLE );
+    gtk_window_set_default_size( GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT );
+    gtk_container_set_border_width(GTK_CONTAINER(window), WINDOW_BORDER);
     
     // Create a notebook with tabs for each of the game's chapter
     GtkWidget *notebook = gtk_notebook_new();
@@ -46,14 +55,49 @@ static void activate( GtkApplication* app, gpointer user_data )
     // Create the groups of save entries on each page
     for (size_t i = 0; i < hb_locations_amount; i++)
     {
+        // Get the current location from the list
         HeartboundLocation my_location = hb_location_list[i];
-        size_t my_world = my_location.world;
-        size_t my_position = my_location.position * 2;
+        size_t my_world = my_location.world;            // To which page it goes
+        size_t my_position = my_location.position * 2;  // Position on the grid
+
+        /* Note:
+            Each location occupies two rows and two columns on the grid.
+            The label goes to the first row (width of two columns).
+            The image goes to the second row, first column.
+            The remaining cell (second row of the second column) will be for for the save's variables.
+        */
         
+        // Create label with the location's name
         GtkWidget *my_label = gtk_label_new(my_location.name);
         gtk_widget_set_halign(my_label, GTK_ALIGN_START);
 
-        GtkWidget *my_image = gtk_image_new_from_file(my_location.image);
+        // Load texture from file
+        GdkPixbuf *my_texture = gdk_pixbuf_new_from_file(my_location.image, NULL);
+        
+        // Scale down the texture if it is bigger than the maximum size
+        // (while keeping the aspect ratio)
+        double my_width = (double)gdk_pixbuf_get_width(my_texture);
+        double my_height = (double)gdk_pixbuf_get_height(my_texture);
+        double my_ratio = my_width / my_height;
+        if (my_height > my_width)
+        {
+            my_height = MIN(my_height, IMAGE_HEIGHT);
+            my_width = my_height * my_ratio;
+        }
+        else
+        {
+            my_width = MIN(my_width, IMAGE_WIDTH);
+            my_height = my_width * (1.0 / my_ratio);
+        }
+
+        GdkPixbuf *my_texture_resized = gdk_pixbuf_scale_simple(my_texture, my_width, my_height, GDK_INTERP_BILINEAR);
+        g_object_unref(my_texture);
+        
+        // Create an image from the texture
+        GtkWidget *my_image = gtk_image_new_from_pixbuf(my_texture_resized);
+        g_object_unref(my_texture_resized);
+
+        // Add the image and labels to the window
         gtk_grid_attach(GTK_GRID(chapter_grid[my_world]), my_label, 0, my_position, 2, 1);
         gtk_grid_attach(GTK_GRID(chapter_grid[my_world]), my_image, 0, my_position+1, 1, 1);
     }
