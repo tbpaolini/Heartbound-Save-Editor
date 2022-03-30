54bae6ab727a0e87ec2970f4516cf8ea777e0eb8
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-03-30 13:19:06 -0300

    Callback function for changing a storyline variable when a radio button is toggled

--- includes/hb_gui_callback.c
+++ includes/hb_gui_callback.c
@@ -6,30 +6,59 @@
 #include <gtk\gtk.h>
 #include <hb_save.h>
 
+// Set a storyline variable's value when one of its radio buttons is clicked
 void hb_setvar_radio_button(GtkRadioButton* widget, StorylineVars *story_var)
 {
+    // Is the button that triggered the 'toggled' event active?
     gboolean button_is_active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget));
     if (!button_is_active) return;
+    /*
+        Since the 'toggled' event is fired by both the button that got unselected
+        and the one that got selected, then we need to check whether the button is
+        active so we don't end up executing the same function twice.
+    */
     
+    // Get the group that the button is part of
     GSList *my_group = gtk_radio_button_get_group(widget);
     GtkToggleButton *current_button = GTK_TOGGLE_BUTTON(my_group->data);
+    /*
+        The group is a linked list. The '->data' attribute is the pointer to
+        a button, and the '->next' attribute the pointer to the next element
+        on the list.
+    */
 
+    // Loop backwards through the possible variable's values
+    // (Because whem iterating through the buttons they come in the opposite
+    //  order they were added: from newest to oldest)
     for (size_t i = story_var->num_entries - 1; i >= 0; i--)
     {
+        // Is the current button active?
         if ( gtk_toggle_button_get_active(current_button) )
         {
+            // Change the storyline variable's value
+            story_var->value = atof(story_var->aliases[i].header[0]);
+            /*
+                The '.header' attribute is a list of the actual values (as strings)
+                that correspond to each alias (the user friendly name).
+            */
+            
+            // Show a message on the console if this is the debug build
             #ifdef _DEBUG
-            g_message("Var %llu -> %s = %s (%s)", story_var->number, story_var->name, story_var->aliases[i].description, story_var->aliases[i].header[0]);
+            g_message(
+                "Var %llu -> %s = %s (%s)",
+                story_var->number,
+                story_var->name,
+                story_var->aliases[i].description,
+                story_var->aliases[i].header[0]
+            );
             #endif
+            
+            // Exit the function when the active button of the group was found
             return;
         }
 
+        // If the button is not active, move to the next one on the group
         my_group = my_group->next;
         current_button = GTK_TOGGLE_BUTTON(my_group->data);
     }
-    
-
-    // gtk_toggle_button_get_active();
-    // hb_save_data[story_var->number].value = story_var->value;
-    // printf("Var %d = %.0f\n", story_var->number, story_var->value);
 }
