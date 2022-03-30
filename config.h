7a6465171851c8da6347b61bc2d4429df5aaf3eb
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-03-30 19:29:41 -0300

    Moved the macros of 'main.c' to a separate file ('config.h')

---
+++ config.h
@@ -0,0 +1,27 @@
+/*
+    Headers for the 'main.c' source file.
+    This file defines the configurations of the main application window.
+*/
+
+// Window properties
+#define WINDOW_WIDTH 720
+#define WINDOW_HEIGHT 540
+#define WINDOW_BORDER 10
+#define WINDOW_TITLE "Heartbound Save Editor"
+#define WINDOW_ICON "..\\lib\\icon.png"
+
+// Page layout
+#define PAGE_BORDER 10
+#define GRID_ROW_SPACING 15
+#define GRID_COLUMN_SPACING 5
+#define IMAGE_WIDTH 80
+#define IMAGE_HEIGHT 80
+#define IMAGE_MARGIN 10
+#define ENTRY_VERTICAL_SPACING 0
+#define ENTRY_HORIZONTAL_SPACING 0
+
+// Text fields properties
+#define TEXT_FIELD_MARGIN 10
+#define TEXT_FIELD_WIDTH 5
+#define TEXT_FIELD_MAX_CHARS 14
+#define TEXT_BUFFER_SIZE 1024
--- main.c
+++ main.c
@@ -4,29 +4,8 @@
 #include <gtk\gtk.h>
 #include <unistd.h>
 #include <hb_save.h>
+#include <config.h>
 // #include <windows.h>
-
-#define WINDOW_WIDTH 720
-#define WINDOW_HEIGHT 540
-#define WINDOW_BORDER 10
-#define WINDOW_TITLE "Heartbound Save Editor"
-#define WINDOW_ICON "..\\lib\\icon.png"
-
-#define PAGE_BORDER 10
-#define GRID_ROW_SPACING 15
-#define GRID_COLUMN_SPACING 5
-#define IMAGE_WIDTH 80
-#define IMAGE_HEIGHT 80
-#define IMAGE_MARGIN 10
-
-#define ENTRY_VERTICAL_SPACING 0
-#define ENTRY_HORIZONTAL_SPACING 0
-
-#define TEXT_FIELD_MARGIN 10
-#define TEXT_FIELD_WIDTH 5
-#define TEXT_FIELD_MAX_CHARS 14
-
-#define TEXT_BUFFER_SIZE 1024
 
 static void activate( GtkApplication* app, gpointer user_data )
 {
