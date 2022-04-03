e312161ef8a5476e3676cf1c2f1afef4f3df1cec
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-04-02 22:32:21 -0300

    Visual tweak

--- main.c
+++ main.c
@@ -320,7 +320,7 @@ static void activate( GtkApplication* app, gpointer user_data )
                                     gtk_widget_set_hexpand(my_wrapper, TRUE);\
                                     my_name_label = gtk_label_new(text);\
                                     gtk_widget_set_valign(my_name_label, GTK_ALIGN_START);\
-                                    gtk_widget_set_margin_end(my_name_label, ENTRY_HORIZONTAL_SPACING + TEXT_FIELD_MARGIN);\
+                                    gtk_widget_set_margin_end(my_name_label, ENTRY_HORIZONTAL_SPACING);\
                                     gtk_widget_set_margin_top(my_name_label, 8);\
                                     gtk_container_add(GTK_CONTAINER(my_wrapper), my_name_label);\
                                     gtk_container_add(GTK_CONTAINER(my_cell), my_wrapper)
@@ -331,6 +331,7 @@ static void activate( GtkApplication* app, gpointer user_data )
         // Create the dropdown list for the room names
         GtkWidget *room_selection = gtk_combo_box_text_new();
         gtk_container_add(GTK_CONTAINER(my_wrapper), room_selection);
+        gtk_widget_set_margin_start(room_selection, TEXT_FIELD_MARGIN);
 
         // Add the rooms to the dropdown list
         size_t current_room_index = 142;    // Default room: home_bedroom
