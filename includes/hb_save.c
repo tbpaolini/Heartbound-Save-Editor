cca7ab6c98bf4cceac5c194a6b01add3a6dbc09d
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-04-14 14:57:42 -0300

    Now the editor opens a Heartbound save passed to it as an argument

--- includes/hb_save.c
+++ includes/hb_save.c
@@ -9,14 +9,43 @@
 
 #include <stdbool.h>
 #include <hb_save.h>
+#include <windows.h>
 
 // Initialize the data structures and parse the save file
-void hb_open_save()
+void hb_open_save(char *path)
 {
     if (hb_save_is_open) return;
     hb_create_save_struct();
     hb_find_save();
-    hb_read_save(SAVE_PATH);    // This function sets 'hb_save_is_open' to 'true' if successful
+    
+    // Try opening the provided path
+    int status = hb_read_save(path);    // This function sets 'hb_save_is_open' to 'true' if successful
+    if (status == SAVE_FILE_NOT_VALID && path != SAVE_PATH)
+    {
+        // Create the text of the error dialog
+        char *message = calloc(TEXT_BUFFER_SIZE, sizeof(char));
+        snprintf(
+            message,
+            TEXT_BUFFER_SIZE,
+            "Coult not open:\n%s\n\n"
+            "The default Heartbound save is going to be opened instead.",
+            path
+        );
+
+        MessageBoxA(
+            NULL,
+            message,
+            "Heartbound Save Editor - error",
+            MB_ICONERROR | MB_OK
+        );
+        
+        // Deallocate the message buffer
+        free(message);
+        
+        // If not possible to open it, then open the default path
+        hb_read_save(SAVE_PATH);
+    }
+
     hb_parse_rooms_locations();
 }
 
