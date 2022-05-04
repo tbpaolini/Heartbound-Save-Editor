c9b4d9a8c109a95ffe1e69badd3d257191cbe323
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-05-03 21:20:38 -0300

    Fixed buffer overflow on the configurations system

--- includes/hb_editor_options.c
+++ includes/hb_editor_options.c
@@ -55,18 +55,36 @@ static uint32_t config_hash(char *key)
 static bool config_add(char *key, char *value)
 {
     // Allocate memory for the setting struct
-    EditorSetting *my_setting = malloc(sizeof(EditorSetting));
+    EditorSetting *my_setting = calloc(1, sizeof(EditorSetting));
     if (my_setting == NULL) return false;  // Stop if there's no available memory
     
     // Store the key/value pair on the struct
-    my_setting->key   = malloc( strnlen_s(key  , EDITOR_CFG_BUFFER) + 1 );
-    if (my_setting->key == NULL) return false;
-    my_setting->value = malloc( strnlen_s(value, EDITOR_CFG_BUFFER) + 1 );
-    if (my_setting->value == NULL) return false;
 
-    strncpy_s(my_setting->key  , EDITOR_CFG_BUFFER, key  , EDITOR_CFG_BUFFER);
-    strncpy_s(my_setting->value, EDITOR_CFG_BUFFER, value, EDITOR_CFG_BUFFER);
+    // Get the size of the key and value
+    size_t key_len = strnlen(key  , EDITOR_CFG_BUFFER);
+    size_t value_len = strnlen(value  , EDITOR_CFG_BUFFER);
+    
+    // Try allocating memory for the key and value
+    my_setting->key   = calloc(key_len + 1, sizeof(char));
+    if (my_setting->key == NULL)
+    {
+        free(my_setting);
+        return false;
+    }
+    
+    my_setting->value = calloc(value_len + 1, sizeof(char));
+    if (my_setting->value == NULL)
+    {
+        free(my_setting);
+        free(my_setting->key);
+        return false;
+    }
 
+    // Copy the key and value to the allocated memory
+    strncpy(my_setting->key  , key  , key_len);
+    strncpy(my_setting->value, value, value_len);
+
+    // Pointers to the next entries on the data structures
     my_setting->list_next = NULL;
     my_setting->map_next  = NULL;
 
@@ -136,7 +154,7 @@ void hb_config_init()
         key   = strtok(line_buffer, "=");
         if (key == NULL)   continue;
         
-        value = strtok(NULL, "=");
+        value = strtok(NULL, "\n");
         if (value == NULL) continue;
 
         // Strip the newline at the end of the value, if there is any
@@ -174,11 +192,12 @@ void hb_config_set(char *key, char *value)
                 char *old_value = current->value;
 
                 // Try to replace the old value
-                current->value = malloc(strnlen_s(value, EDITOR_CFG_BUFFER) + 1);
+                size_t value_len = strnlen(value, EDITOR_CFG_BUFFER);
+                current->value = calloc(value_len + 1, sizeof(char));
                 if (current->value != NULL)
                 {
                     // Store the new value and delete the old one
-                    strncpy_s(current->value, EDITOR_CFG_BUFFER, value, EDITOR_CFG_BUFFER);
+                    strncpy(current->value, value, value_len);
                     free(old_value);
                     config_write();
                 }
@@ -360,5 +379,6 @@ void hb_config_close()
         free(previous_config);
     }
 
+    free(EDITOR_CFG_LOCATION);
     config_count = 0;
 }
