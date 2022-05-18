7b934c247e44b62951792e72c0f3ac1721fe588f
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-05-18 16:54:52 -0300

    Updated version number to 1.0.0.4 on the source files No pre-built binaries are available yet, because the next version of Heartbound has not been released yet.

--- includes/hb_gui_callback.c
+++ includes/hb_gui_callback.c
@@ -1805,7 +1805,7 @@ void hb_menu_help_about(GtkMenuItem *widget, GtkWindow *window)
         window,
         "logo", logo,
         "program-name", "Heartbound Save Editor",
-        "version", "Version 1.0.0.3",
+        "version", "Version 1.0.0.4",
         "authors", authors,
         "documenters", documenters,
         "copyright", "Copyright 2022 by Tiago Becerra Paolini",
