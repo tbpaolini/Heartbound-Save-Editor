90af1353fbcca83f18c25c0df359f64f26cd2d65
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-06-14 02:05:52 -0300

    Rooms now get their index assigned properly The index of the rooms must start from zero and always increase by one. But thanks to an oversight on the commit previous commit of room_sorting.py (1e920003bc9f578522737baca0254b8cd590b49a), the index kept increasing even if a room was skipped.

--- utils/room_sorting.py
+++ utils/room_sorting.py
@@ -10,5 +10,10 @@ with open("..\\structure\\Save File Structure - Room IDs.tsv", "rt") as rooms_fi
 
 with open("..\\structure\\room_coordinates.tsv", "wt") as output_file:
     output_file.write("Index\tRoom\tx-axis\ty-axis\n")
-    lines = [f"{index}\t" + "\t".join(room) for index, room in enumerate(sorted(rows)) if room[0] in rooms_list]
+    lines = []
+    index = 0
+    for room in sorted(rows):
+        if room[0] in rooms_list:
+            lines.append(f"{index}\t" + "\t".join(room))
+            index += 1
     output_file.writelines(lines)
