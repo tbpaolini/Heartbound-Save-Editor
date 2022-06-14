1e920003bc9f578522737baca0254b8cd590b49a
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-06-14 00:50:52 -0300

    Added code to check which rooms still exist ingame

--- utils/room_sorting.py
+++ utils/room_sorting.py
@@ -5,7 +5,10 @@ Python script for sorting alphabetically the rooms that were mapped by 'room map
 with open("..\\structure\\room_coordinates-raw.tsv", "rt") as input_file:
     rows = [line.split("\t") for line in input_file]
 
+with open("..\\structure\\Save File Structure - Room IDs.tsv", "rt") as rooms_file:
+    rooms_list = set(room.strip() for room in rooms_file.readlines())
+
 with open("..\\structure\\room_coordinates.tsv", "wt") as output_file:
     output_file.write("Index\tRoom\tx-axis\ty-axis\n")
-    lines = [f"{index}\t" + "\t".join(room) for index, room in enumerate(sorted(rows))]
+    lines = [f"{index}\t" + "\t".join(room) for index, room in enumerate(sorted(rows)) if room[0] in rooms_list]
     output_file.writelines(lines)
