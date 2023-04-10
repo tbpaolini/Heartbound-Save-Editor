575da1630d6fb0f88721a6d7e700518db1c7a05f
Tiago Becerra Paolini <tpaolini@gmail.com>
2023-04-10 13:19:29 -0300

    Removing newline character when parsing the options file

--- includes/hb_game_options.c
+++ includes/hb_game_options.c
@@ -111,6 +111,7 @@ void hb_read_game_options()
             {
                 // If we arrived at the end of the line without invalid characters
                 is_valid = true;
+                line_buffer[pos] = '\0';    // Remove the newline character
                 break;
             }
 
